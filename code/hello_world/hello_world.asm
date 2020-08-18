373baae0631a032c63b66a259b8181c39723a239
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-08-18 22:08:16 +0200

    Update hello_world.asm

--- code/hello_world/hello_world.asm
+++ code/hello_world/hello_world.asm
@@ -9,12 +9,12 @@ section .data
 section .text
     global _main
 _main:
-    mov rax, 0x2000004      ; 4 - sys_write
-    mov rdi, 1              ; arg 1: destination. 1 = STDOUT
-    mov rsi, msg            ; arg 2: char *
-    mov rdx, 13             ; arg 3: len of output
+    mov rax, 0x02000004     ; 4 - sys_write
+    mov rdi, 1              ; 1st arg : destination. 1 = STDOUT
+    mov rsi, msg            ; 2sd arg : char *
+    mov rdx, 13             ; 3rd arg : len of output
     syscall
         
-    mov rax, 0x2000001      ; 1 - sys_exit
+    mov rax, 0x02000001     ; 1 - sys_exit
     xor rdi, rdi            ; return code = 0
     syscall
