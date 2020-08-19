b33f4b5314ab35f62d4d16d5c33a23aef8c1195a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-08-19 19:19:06 +0200

    Create hello_world_printf.asm

---
+++ code/hello_world_printf/hello_world_printf.asm
@@ -0,0 +1,25 @@
+; NASM assembly for Mac OS X x64
+; hello_world_printf.asm
+; prints hello, world! using external C printf.
+; Hudson Schumaker
+
+extern _printf
+section .data
+    msg:   db   "hello, world!", 0x0  ; in c "string" ends with 0
+    fmtstr db	"%s", 0xA, 0x0          ; format for printing a string
+
+section .text
+    global _main
+
+_main:
+    push rbp
+    mov  rbp, rsp
+
+    mov  rax, 0       ; no floating point
+    mov  rdi, fmtstr  ; 1st arg : destination.
+    mov  rsi, msg     ; 2sd arg : char *
+    call _printf
+
+    mov  rax, 0x02000001     ; 1 - sys_exit
+    xor  rdi, rdi            ; return code = 0
+    syscall
