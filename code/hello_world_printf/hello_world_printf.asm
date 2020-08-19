b4331c43631f868ff27838fbe6188c301d25ef0c
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-08-19 19:39:14 +0200

    Update hello_world_printf.asm

--- code/hello_world_printf/hello_world_printf.asm
+++ code/hello_world_printf/hello_world_printf.asm
@@ -3,23 +3,23 @@
 ; prints hello, world! using external C printf.
 ; Hudson Schumaker
 
-extern _printf
-section .data
-    msg:   db   "hello, world!", 0x0  ; in c "string" ends with 0
-    fmtstr db	"%s", 0xA, 0x0          ; format for printing a string
+extern _printf                        ; external function of C 
+section .data                         ; your data here
+    msg:    db   "hello, world!", 0x0 ; in c "string" ends with 0
+    fmtstr: db.  "%s", 0xA, 0x0       ; format for printing a string
 
-section .text
+section .text   ; your code here
     global _main
 
 _main:
-    push rbp
-    mov  rbp, rsp
+    push    rbp
+    mov     rbp, rsp
 
-    mov  rax, 0       ; no floating point
-    mov  rdi, fmtstr  ; 1st arg : destination.
-    mov  rsi, msg     ; 2sd arg : char *
-    call _printf
+    mov     rax, 0       ; no floating point
+    mov     rdi, fmtstr  ; 1st arg : destination.
+    mov     rsi, msg     ; 2sd arg : char *
+    call    _printf
 
-    mov  rax, 0x02000001     ; 1 - sys_exit
-    xor  rdi, rdi            ; return code = 0
+    mov     rax, 0x02000001   ; 1 - sys_exit
+    xor     rdi, rdi          ; return code = 0
     syscall
