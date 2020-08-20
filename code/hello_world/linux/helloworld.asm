9f543fae51163950879ef4fed6c997e72e5fc1a2
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-08-20 13:39:40 +0200

    Create helloworld.asm

---
+++ code/hello_world/linux/helloworld.asm
@@ -0,0 +1,21 @@
+bits 64
+
+section .data
+	msg db  "Hello World Fedora 32 from nasm", 0x0
+	len equ	$ - msg 
+
+section .bss
+
+section .text
+        global main
+
+main:
+	mov  rax, 1
+	mov  rdi, 1
+	mov  rsi, msg
+	mov  rdx, len
+	syscall
+
+	mov  rax, 60
+	mov  rdi, 0
+	syscall 
