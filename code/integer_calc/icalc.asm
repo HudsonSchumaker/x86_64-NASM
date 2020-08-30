aee50188ce46aa44950eef52ddd642f6c5dbbed9
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-08-30 13:13:32 +0200

    Create icalc.asm

---
+++ code/integer_calc/icalc.asm
@@ -0,0 +1,36 @@
+; O3 Ozone Project Lab
+; icalc.asm
+; Hudson Schumaker
+
+bits 64
+
+section .data
+section .bss
+section .text
+  global _o3add, _o3sub, _o3mult, _o3pow, _o3div ; entry points
+
+_o3add:
+  mov	 rax, rdi  ; 1st arg, moved to rax
+  add	 rax, rsi  ; 2sd arg, added to rax(rdi value)
+  ret            ; return, rax has the sum value
+
+_o3sub:
+  mov	 rax, rdi  ; 1st arg, moved to rax
+  sub	 rax, rsi  ; 2sd arg, subtracted from rax
+  ret            ; return, rax has the subtraction value
+
+_o3mult:
+  mov	 rax, rdi  ; 1st arg, moved to rax
+  imul rsi       ; 2sd arg, multiply rsi with rax (rdi vale)
+  ret            ; return, rax has the result
+
+_o3pow:
+  mov  rax, rdi‚ ; 1st arg, moved to rax
+  imul rdi       ; 2sd arg, rax(rdi) * rdi -> rdi * rdi
+  ret            ; return, rax has the result
+
+_o3div:
+  mov  rax, rdi  ; 1st arg, moved to rax
+  mov  rdx, 0    ; rdx needs to be 0 before idiv
+  idiv rsi       ; 2sd arg, divide rax(rdi value) by rsi
+  ret            ; return, rax has the result
